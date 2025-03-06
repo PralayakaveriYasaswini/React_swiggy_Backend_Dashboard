05da1631e0f462f0e0a83c4cd0eb8d9b2ed16939
PRALAYAKAVERI YASASWINI <163825626+PralayakaveriYasaswini@users.noreply.github.com>
2025-03-06 23:40:31 +0530

    react backend dashboard

---
+++ src/vendorDashboard/components/Welcome.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Welcome = () => {
+    
+  return (
+    <div className='welcomeSection'>
+        <h2>Welcome </h2>
+        
+    </div>
+  )
+}
+
+export default Welcome

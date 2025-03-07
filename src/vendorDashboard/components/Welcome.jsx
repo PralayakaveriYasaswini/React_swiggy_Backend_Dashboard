484375de3734d45d1c007f3081b16effb7c473ec
PRALAYAKAVERI YASASWINI <163825626+PralayakaveriYasaswini@users.noreply.github.com>
2025-03-07 21:25:24 +0530

    changed src file

--- src/vendorDashboard/components/Welcome.jsx
+++ src/vendorDashboard/components/Welcome.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
 const Welcome = () => {
-    
+    const firmName = localStorage.getItem("firmName")
+
   return (
     <div className='welcomeSection'>
-        <h2>Welcome </h2>
-        
+        <h2>Welcome {firmName}</h2>
+        <div className="landingImage">
+          <img src='/assets/chef.jpg' alt='welcome' />
+        </div>
     </div>
   )
 }
